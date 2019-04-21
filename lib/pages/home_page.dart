c134ba6956da11dff8c57523966abd109221ceda
emiliazhen <esc100@vip.qq.com>
2019-04-21 21:18:33 +0800

    :art: UI update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
-class PageHome extends StatelessWidget {
-  void getHttp() async{
-    try {
-      Response res;
-      res = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=002');
-      print(res);
-    } catch (e) {
-    }
-  }
+import '../config/httpHeaders.dart';
 
+class PageHome extends StatefulWidget {
+  @override
+  _PageHomeState createState() => _PageHomeState();
+}
+
+class _PageHomeState extends State<PageHome> {
+  String showText = '还没有请求数据';
   @override
   Widget build(BuildContext context) {
-    getHttp();
-    return Scaffold(
-      body: Center(
-        child: Text('首页'),
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(title: Text('请求远程数据'),),
+        body:SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: _jike,
+                child: Text('请求数据'),
+              ),
+              Text(showText)
+            ],
+          ),
+        )
       ),
     );
+  }
+
+  void _jike(){
+    print('开始向极客时间请求数据...');
+    getHttp().then((val){
+      setState(() {
+       showText = val['data'].toString();
+      });
+    });
+  }
+
+  Future getHttp() async {
+    try{
+      Response res;
+      Dio dio = new Dio();
+      dio.options.headers = httpHeaders;
+      res = await dio.get('https://time.geekbang.org/serv/v1/column/newAll');
+      print(res);
+      return res.data;
+    }catch(e){
+      return print(e);
+    }
   }
 }
