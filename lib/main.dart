3dc5b600a3093be4a9abc371597f13efcfd6beac
emiliazhen <esc100@vip.qq.com>
2019-05-01 14:07:25 +0800

    :heavy_plus_sign: Screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: MaterialApp(
-        title: 'Big 生 活 Shop+',
+        title: 'LifeShop',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primaryColor: Colors.pink
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'dart:convert';
 
 class PageHome extends StatefulWidget {
@@ -13,16 +14,21 @@ class _PageHomeState extends State<PageHome> {
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        appBar: AppBar(title: Text('请求远程数据'),),
+        appBar: AppBar(title: Text('LifeShop+'),),
         body: FutureBuilder(
           future: getHomePageContent(),
           builder: (context,snapshot){
             if(snapshot.hasData){
               var data = json.decode(snapshot.data.toString());
               List<Map> swiperDataList = (data['data']['slides'] as List).cast();
+              List<Map> navigatorList = (data['data']['category'] as List).cast();
+              if(navigatorList.length > 10){
+                navigatorList.removeRange(10, navigatorList.length);
+              }
               return Column(
                 children: <Widget>[
-                  SwiperDiy(swiperDataList: swiperDataList)
+                  SwiperDiy(swiperDataList: swiperDataList),
+                  TopNavigator(navigatorList: navigatorList)
                 ],
               );
             } else {
@@ -42,14 +48,52 @@ class SwiperDiy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 333,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
       child: Swiper(
         itemBuilder: (BuildContext context,int index){
           return Image.network('${swiperDataList[index]['image']}',fit: BoxFit.fill);
         },
-        itemCount: 3,
+        itemCount: swiperDataList.length,
         pagination: new SwiperPagination(),
         autoplay: true,
+      ),
+    );
+  }
+}
+
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+  TopNavigator({Key key,this.navigatorList}):super(key:key);
+
+  Widget _gridViewItemUI(BuildContext context,item){
+    return InkWell(
+      onTap: (){
+        print('---Tap---');
+      },
+      child: Column(
+        children: <Widget>[
+          Image.network(
+            item['image'],
+            width:ScreenUtil().setWidth(95)
+          ),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(4.0),
+        children: navigatorList.map((item){
+          return _gridViewItemUI(context,item);
+        }).toList()
       ),
     );
   }
