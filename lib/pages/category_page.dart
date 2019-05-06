a05b4e2cf41195e023d636fcc5005b2e0534fd09
emiliazhen <esc100@vip.qq.com>
2019-05-06 22:51:03 +0800

    :lipstick: CategoryPage

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class PageCategory extends StatelessWidget {
+import '../service/service_method.dart';
+
+class PageCategory extends StatefulWidget {
+  @override
+  _PageCategoryState createState() => _PageCategoryState();
+}
+
+class _PageCategoryState extends State<PageCategory> {
+  void _getCategory() async {
+    await request('getCategory').then((res){
+      var data = json.decode(res.toString());
+      print(data);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('分类'),
-      ),
+    _getCategory();
+    return Container(
+      child: Text('分类'),
     );
   }
 }
