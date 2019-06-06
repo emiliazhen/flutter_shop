dba0bff0afaa974260dd73e7e3f9255fa69f6bcf
emiliazhen <esc100@vip.qq.com>
2019-06-06 21:27:48 +0800

    :bug: Delete print

--- lib/routers/router_handler.dart
+++ lib/routers/router_handler.dart
@@ -5,7 +5,6 @@ import '../pages/goods_detail_page.dart';
 Handler goodsDetailHandler = Handler(
   handlerFunc: (BuildContext context,Map<String,List<String>> params){
     String goodsId = params['id'].first;
-    print('detailId is $goodsId');
     return GoodsDetailPage(goodsId);
   }
 );
